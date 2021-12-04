ebca85d4d72e95d6778e0d499c2fd65be6acfb70
suzhetao <suzhetao@gmail.com>
2021-12-04 16:20:15 +0800

    new file:   metrics-server/README.md 	new file:   metrics-server/backend.tf 	new file:   metrics-server/helm.tf 	new file:   metrics-server/metrics-server.yaml 	new file:   metrics-server/provider.tf 	new file:   metrics-server/terragrunt.hcl 	new file:   metrics-server/variables.tf

---
+++ metrics-server/variables.tf
@@ -0,0 +1,25 @@
+variable "deploy_spec" {
+  default = {
+    namespace = {
+      name = "kube-system"
+      annotations = {}
+      labels = {}
+    }
+    helm = {
+      metrics-server = {
+        repository = "https://kubernetes-sigs.github.io/metrics-server"
+        chart = "metrics-server"
+        version = "3.7.0"
+        values = "metrics-server.yaml"
+      }
+    }
+  }
+}
+
+variable "annotations" {
+  default = {}
+}
+
+variable "labels" {
+  default = {}
+}
