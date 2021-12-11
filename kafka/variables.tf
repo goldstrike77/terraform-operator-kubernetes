2fcc4e8510930b265b651afd4d2b476747176609
oper <oper@dem-p-inf-mas01.node.dc01.local>
2021-12-11 20:31:29 +0800

    modified:   kafka/kafka.yaml 	new file:   kafka/kowl.yaml 	modified:   kafka/variables.tf 	modified:   loki/loki-distributed.yaml

--- kafka/variables.tf
+++ kafka/variables.tf
@@ -9,8 +9,14 @@ variable "deploy_spec" {
       kafka = {
         repository = "https://charts.bitnami.com/bitnami"
         chart = "kafka"
-        version = "14.4.3"
+        version = "14.5.0"
         values = "kafka.yaml"
+      },
+      kowl = {
+        repository = "https://raw.githubusercontent.com/cloudhut/charts/master/archives"
+        chart = "kowl"
+        version = "2.1.0"
+        values = "kowl.yaml"
       }
     }
   }
