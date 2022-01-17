ac64c286b38623159baf91fbbb11f2bae33a8e60
oper <oper@clu-p-cen-oss01.node.dc01.local>
2022-01-17 18:19:35 +0800

    Initial commit

--- prometheus/norther/variables.tf
+++ prometheus/norther/variables.tf
@@ -6,11 +6,23 @@ variable "deploy_spec" {
       labels = {}
     }
     helm = {
-      kube-prometheus = {
+      thanos = {
         repository = "https://charts.bitnami.com/bitnami"
-        chart = "../../_chart/kube-prometheus-6.5.3.tgz"
-        version = "6.5.3"
-        values = "kube-prometheus.yaml"
+        chart = "../../_chart/thanos-8.3.0.tgz"
+        version = "8.3.0"
+        values = "thanos.yaml"
+      },
+      kube-state-metrics = {
+        repository = "https://prometheus-community.github.io/helm-charts"
+        chart = "kube-state-metrics"
+        version = "4.4.0"
+        values = "kube-state-metrics.yaml"
+      },
+      kube-prometheus = {
+        repository = "https://prometheus-community.github.io/helm-charts"
+        chart = "kube-prometheus-stack"
+        version = "30.0.1"
+        values = "kube-prometheus-stack.yaml"
       }
     }
   }
